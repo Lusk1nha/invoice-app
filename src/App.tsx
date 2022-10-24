28014129cc32efd63f9194ae70f8bc88ea893fa9
unknown <lucaspedro517@gmail.com>
2022-10-24 20:03:30 -0300

    Creating project with Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-purple-600 color-black font-bold space-x-4">
+      Teste
+    </div>
+  )
+};
